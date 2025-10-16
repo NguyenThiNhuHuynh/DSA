a49e8baa5308985ac814f6bc54d0931eb1697f33
22520592 <22520592@gm.uit.edu.vn>
2025-10-16 18:20:31 +0700

    is Valid Sudoku

---
+++ ArrayAndHashing/ValidSudoku/ValidSudoku/ValidSudoku.cpp
@@ -0,0 +1,65 @@
+﻿// ValidSudoku.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+using namespace std;
+
+class Solution {
+public: 
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int, unordered_set<char>> cols, row;
+        map<pair<int, int>, unordered_set<char>> squares;
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') {
+                    continue;
+                }
+                pair<int, int> squareKey = { r / 3,c / 3 };
+                if (cols[c].count(board[r][c]) || row[r].count(board[r][c]) || squares[squareKey].count(board[r][c])) {
+                    return false;
+                }
+                cols[c].insert(board[r][c]);
+                row[r].insert(board[r][c]);
+                squares[squareKey].insert(board[r][c]);
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    vector<vector<char>> board = {
+         {'5','3','.','.','7','.','.','.','.'},
+         {'6','.','.','1','9','5','.','.','.'},
+         {'.','9','8','.','.','.','.','6','.'},
+         {'8','.','.','.','6','.','.','.','3'},
+         {'4','.','.','8','.','3','.','.','1'},
+         {'7','.','.','.','2','.','.','.','6'},
+         {'.','6','.','.','.','.','2','8','.'},
+         {'.','.','.','4','1','9','.','.','5'},
+         {'.','.','.','.','8','.','.','7','9'}
+    };
+
+    Solution sol;
+    bool valid = sol.isValidSudoku(board);
+
+    cout << "Board hop le " << (valid ? "dung ✅" : "sai ❌") << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
